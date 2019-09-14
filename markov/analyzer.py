c2fc4c8d2c61af83f5bce07aed00b420e915e61b
Ayanami Akaha <ayanami@akaha.today>
2019-09-15 02:18:44 +0900

    Fixes and refactor

--- markov/analyzer.py
+++ markov/analyzer.py
@@ -129,3 +129,4 @@ class Analyzer:
       if current[-1:] == '.':
         self.__upsert(current, None)
         last = None
+    self._db.commit()
--- markov/generator.py
+++ markov/generator.py
@@ -8,6 +8,18 @@ SELECT = {
   'pyformat': r'SELECT * FROM markov WHERE w1=%(w1)s ORDER BY cnt',
 }
 
+def random_from(wordstats):
+  """
+  Selects random word from the array of tuples given.
+  :param wordstats array of tuples (firstword, secondword, count)
+  :returns randomly selected word
+  """
+  total = reduce((lambda sum, (w1, w2, cnt): sum+cnt), wordstats, 0)
+  rnd = random.randint(0, total-1)
+  for _, w, cnt in wordstats:
+    rnd -= cnt
+    if rnd < 0: return w
+
 class Generator:
   """
   Generates random texts using Markov chains principle, from given
@@ -19,29 +31,23 @@ class Generator:
     self._db = db.connect(connstr)
     self.last_word = None
     self.SELECT = SELECT[db.paramstyle]
+    self.SELECT_FIRST = 'SELECT * FROM markov WHERE w1 IS NULL ORDER BY cnt'
     self._cursor = self._db.cursor()
 
-  def close():
+  def close(self):
     self._cursor.close()
     self._db.close()
 
-  def random_from(wordstats):
-    """
-    Selects random word from the array of tuples given.
-    :param wordstats array of tuples (firstword, secondword, count)
-    :returns randomly selected word
-    """
-    total = reduce((lambda sum, (_, _, cnt): sum+cnt), wordstats, 0)
-    rnd = random.randint(0, total)
-    for _, w, cnt in wordstats:
-      rnd -= cnt
-      if rnd < 0: return w
-
-  def next_word():
+  def next_word(self):
     """
     Generates next word in the chain
     """
-    wordstats = self._cursor.execute(self.SELECT, (self.last_word,))
-    w = random_from(wordstats)
+    if self.last_word:
+      self._cursor.execute(self.SELECT, (self.last_word,))
+    else:
+      self._cursor.execute(self.SELECT_FIRST)
+    wordstat = self._cursor.fetchall()
+    print wordstat
+    w = random_from(wordstat)
     self.last_word = w
     return w
